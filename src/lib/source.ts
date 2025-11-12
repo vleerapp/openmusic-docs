73ca1fb9e7dfc0e57f189c41d2c648426624efdd
0PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2025-11-12 11:31:04 +0100

    feat: add explanation page and inital content on index

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -3,24 +3,13 @@ import { type InferPageType, loader } from 'fumadocs-core/source';
 import { Icon } from '@iconify/react';
 import { createElement } from 'react';
 
-// See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
   baseUrl: '/',
   source: docs.toFumadocsSource(),
   icon(icon) {
     if (!icon) return;
 
-    // Iconify icons should be in format "prefix:icon-name"
-    // For example: "lucide:home", "mdi:account"
-    // Browse icons at https://icon-sets.iconify.design/
-    if (!icon.includes(':')) {
-      console.warn(
-        `[iconify] Icon name "${icon}" should be in format "prefix:icon-name". ` +
-        `For example: "lucide:home", "mdi:account". Visit https://icon-sets.iconify.design/`
-      );
-      return;
-    }
-
+    // iconpack:iconname
     return createElement(Icon, { icon });
   },
 });
