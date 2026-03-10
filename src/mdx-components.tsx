eab196cf620c03bb0612e2519105484aebe7f2e5
Waradu <waradu@outlook.com>
2026-03-10 10:13:20 +0100

    fix: unknown component

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,16 +1,28 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import { createAPIPage } from "fumadocs-openapi/ui";
+import type { ComponentProps } from "react";
 import { openmetadata, openmusic } from "@/scripts/generate-docs";
 
 const OpenMusicAPIPage = createAPIPage(openmusic);
 const OpenMetadataAPIPage = createAPIPage(openmetadata);
+type APIPageProps = ComponentProps<typeof OpenMusicAPIPage>;
+
+function APIPage({ document, ...props }: APIPageProps) {
+  switch (document) {
+    case "./public/openmusic.yml":
+      return <OpenMusicAPIPage document={document} {...props} />;
+    case "./public/openmetadata.yml":
+      return <OpenMetadataAPIPage document={document} {...props} />;
+    default:
+      throw new Error(`Unsupported OpenAPI document: ${document}`);
+  }
+}
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    OpenMusicAPIPage,
-    OpenMetadataAPIPage,
+    APIPage,
     ...components,
   };
 }
